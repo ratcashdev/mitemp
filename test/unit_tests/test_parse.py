6679aeed8b26db2688b75a4606834e8e564e2b47
Fabian Bader <fabian.bader@aequitas-integration.de>
2019-01-23 23:13:54 +0100

    Use bytearray instead of bytes

--- test/unit_tests/test_parse.py
+++ test/unit_tests/test_parse.py
@@ -16,8 +16,8 @@ class KNXConversionTest(unittest.TestCase):
     def test_parsing(self):
         """Does the Mi TEMP BT data parser works correctly for positive double digit values? Value: 'T=25.6 H=23.6'"""
         poller = MiTempBtPoller(None, MockBackend)
-        data = bytes([0x54, 0x3d, 0x32, 0x35, 0x2e, 0x36, 0x20,
-                      0x48, 0x3d, 0x32, 0x33, 0x2e, 0x36, 0x00])
+        data = bytearray([0x54, 0x3d, 0x32, 0x35, 0x2e, 0x36, 0x20,
+                      0x48, 0x3d, 0x32, 0x33, 0x2e, 0x36, 0x00]).decode("utf-8").strip(' \n\t')
         poller._cache = data
         poller._last_read = datetime.now()
         self.assertEqual(poller._parse_data()[MI_TEMPERATURE], 25.6)
@@ -26,8 +26,8 @@ class KNXConversionTest(unittest.TestCase):
     def test_parsing(self):
         """Does the Mi TEMP BT data parser works correctly for positive single digit values? Value: T=2.3 H=3.6"""
         poller = MiTempBtPoller(None, MockBackend)
-        data = bytes([0x54, 0x3d, 0x32, 0x2e, 0x33, 0x20,
-                      0x48, 0x3d, 0x33, 0x2e, 0x36])
+        data = bytearray([0x54, 0x3d, 0x32, 0x2e, 0x33, 0x20,
+                      0x48, 0x3d, 0x33, 0x2e, 0x36]).decode("utf-8").strip(' \n\t')
         poller._cache = data
         poller._last_read = datetime.now()
         self.assertEqual(poller._parse_data()[MI_TEMPERATURE], 2.3)
@@ -36,8 +36,8 @@ class KNXConversionTest(unittest.TestCase):
     def test_parsing(self):
         """Does the Mi TEMP BT data parser works correctly for negative single digit values? Value: T=-9.3 H=36.6"""
         poller = MiTempBtPoller(None, MockBackend)
-        data = bytes([0x54, 0x3d, 0x2d, 0x39, 0x2e, 0x33, 0x20,
-                      0x48, 0x3d, 0x33, 0x36, 0x2e, 0x36])
+        data = bytearray([0x54, 0x3d, 0x2d, 0x39, 0x2e, 0x33, 0x20,
+                      0x48, 0x3d, 0x33, 0x36, 0x2e, 0x36]).decode("utf-8").strip(' \n\t')
         poller._cache = data
         poller._last_read = datetime.now()
         self.assertEqual(poller._parse_data()[MI_TEMPERATURE], -9.3)
@@ -46,8 +46,8 @@ class KNXConversionTest(unittest.TestCase):
     def test_parsing(self):
         """Does the Mi TEMP BT data parser works correctly for negative double digit values? Value: T=-11.3 H=37.6"""
         poller = MiTempBtPoller(None, MockBackend)
-        data = bytes([0x54, 0x3d, 0x2d, 0x31, 0x31, 0x2e, 0x33, 0x20,
-                      0x48, 0x3d, 0x33, 0x37, 0x2e, 0x36])
+        data = bytearray([0x54, 0x3d, 0x2d, 0x31, 0x31, 0x2e, 0x33, 0x20,
+                      0x48, 0x3d, 0x33, 0x37, 0x2e, 0x36]).decode("utf-8").strip(' \n\t')
         poller._cache = data
         poller._last_read = datetime.now()
         self.assertEqual(poller._parse_data()[MI_TEMPERATURE], -11.3)
