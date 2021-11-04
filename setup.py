2f7627cf32b9f4102e1a0e1a7a6b0341d305b5eb
ratcash <developer@ratcash.dev>
2021-11-04 13:09:21 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mitemp_bt',
-    version='0.0.4',
+    version='0.0.5',
     description='Library to read data from Mi Temperature and Humidity Sensor (V2) using Bluetooth LE with LCD display',
     url='https://github.com/ratcashdev/mitemp',
     author='ratcashdev',
@@ -16,7 +16,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.9'
     ],
     packages=find_packages(exclude=["test.*", "test"]),
